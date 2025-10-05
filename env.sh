ebe6d9b6a7e98ff1943446136656a1c38dfdfe77
Student <student@example.com>
2025-10-05 10:34:41 +0530

    env added

---
+++ env.sh
@@ -0,0 +1,5 @@
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_S3_FORCE_PATH_STYLE=true
+export AWS_ENDPOINT_URL=http://localhost:4566
