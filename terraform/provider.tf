be52fdee73f034217a65f543f4ce38130443cb9d
Yousaf <yousaf.k.hamza@gmail.com>
2025-12-27 09:49:54 +0400

    update repo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,8 +10,6 @@ terraform {
 
 provider "aws" {
   region                      = var.region
-  access_key                  = "test"
-  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
